7f09829f3acbe792ec3f327da38be3bb2819adbd
4johnny <4johnny@users.noreply.github.com>
2015-01-15 10:31:11 -0800

    Initial Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Johnny on 2015-01-15.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
